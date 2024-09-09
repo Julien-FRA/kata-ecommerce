1c39667ba2746afc9c11862ea918a7e68a1cccc2
Julien-FRA <julien.francois@hetic.net>
2024-09-09 14:47:33 +0200

    <feat/client>: Ajout de la section about

---
+++ client/src/components/Container/About/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+
+export const AboutContainer = () => {
+  return (
+    <div className="about-container">
+      <h2>A propos ?</h2>
+      <p>
+        KOKORO, du japonais signifie à la fois le cœur, l’âme et l’esprit soit
+        les lignes directrices avec lesquelles le projet s’est construit et
+        continue aujourd’hui d’avancer.
+      </p>
+    </div>
+  );
+};
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Hero } from "../components/Hero";
 import { DiscoverContainer } from "../components/Container/Discover";
+import { AboutContainer } from "../components/Container/About";
 
 export const Home = () => {
   return (
     <>
       <Hero />
       <DiscoverContainer />
+      <AboutContainer />
     </>
   );
 };
