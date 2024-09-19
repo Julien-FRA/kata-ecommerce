2b001661094631204963f0f769269f4f295af0ff
Julien-FRA <julien.francois@hetic.net>
2024-09-19 11:04:18 +0200

    <feat/server>: Ajout de la login route

--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -4,6 +4,6 @@ const UserController = require("../controllers/User");
 const router = express.Router();
 
 router.post("/register", UserController.register);
-// router.post("/login", UserController.login);
+router.post("/login", UserController.login);
 
 module.exports = router;
