c6da39b69edb1cbe786c9b3809827c22a7ac9e0a
Julien-FRA <julien.francois@hetic.net>
2024-10-03 10:02:55 +0200

    <feat/server>: Récupération users

--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -3,6 +3,7 @@ import express from "express";
 const UserController = require("../controllers/User");
 const router = express.Router();
 
+router.get("/", UserController.getAllUsers);
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 
